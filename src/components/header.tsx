a23ab9c3ef66de93eec56d62b4c363a2d23326d1
Iriboneye-Nina <ninairiboneye@gmail.com>
2024-08-08 16:48:52 +0200

    setup-ant-design

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
+    <header className="fixed top-0 left-0 w-full flex justify-between items-center p-4 bg-gray-800 text-white shadow-md z-50">
       <div className="text-2xl font-bold">
         <span>Houses</span>
       </div>
       <nav>
         <ul className="flex space-x-4">
           <li>
-            <a href="#" className="hover:text-green-500 transition duration-300">Home</a>
+            <a href="#" className="hover:text-blue-500 transition duration-300">Home</a>
           </li>
           <li>
-            <a href="#" className="hover:text-green-500 transition duration-300">About</a>
+            <a href="#" className="hover:text-blue-500 transition duration-300">About</a>
           </li>
         </ul>
       </nav>
@@ -21,5 +21,3 @@ const Header = () => {
 }
 
 export default Header;
-
-
